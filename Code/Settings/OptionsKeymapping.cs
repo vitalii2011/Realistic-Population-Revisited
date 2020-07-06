9455df5d44b9f3d9f12755f57d8667e43e6535ee
algernon <aeneas@aloku.net>
2020-07-06 14:36:06 +1000

    Add custom mouse button keybinding option.

--- Code/Settings/OptionsKeymapping.cs
+++ Code/Settings/OptionsKeymapping.cs
@@ -84,16 +84,8 @@ namespace RealisticPopulationRevisited
                     inputKey = (keyEvent.keycode == KeyCode.Backspace) ? SavedInputKey.Empty : SavedInputKey.Encode(keyEvent.keycode, keyEvent.control, keyEvent.shift, keyEvent.alt);
                 }
 
-                // Apply settings and save.
-                CurrentHotkey = inputKey;
-                SettingsUtils.SaveSettings();
-
-                // Set the label for the new hotkey.
-                button.text = SavedInputKey.ToLocalizedString("KEYNAME", inputKey);
-
-                // Remove priming.
-                UIView.PopModal();
-                isPrimed = false;
+                // Apply our new key.
+                ApplyKey(inputKey);
             }
         }
 
@@ -110,15 +102,50 @@ namespace RealisticPopulationRevisited
             // Check to see if we're already primed for hotkey entry.
             if (isPrimed)
             {
-                // We were already primed; reset the button text and cancel priming.
-                button.text = SavedInputKey.ToLocalizedString("KEYNAME", CurrentHotkey);
-                UIView.PopModal();
-                isPrimed = false;
+                // We were already primed; is this a bindable mouse button?
+                if (mouseEvent.buttons == UIMouseButton.Left || mouseEvent.buttons == UIMouseButton.Right)
+                {
+                    // Not a bindable mouse button - set the button text and cancel priming.
+                    button.text = SavedInputKey.ToLocalizedString("KEYNAME", CurrentHotkey);
+                    UIView.PopModal();
+                    isPrimed = false;
+                }
+                else
+                {
+                    // Bindable mouse button - do keybinding as if this was a keystroke.
+                    KeyCode mouseCode;
+
+                    switch (mouseEvent.buttons)
+                    {
+                        // Convert buttons to keycodes (we don't bother with left and right buttons as they're non-bindable).
+                        case UIMouseButton.Middle:
+                            mouseCode = KeyCode.Mouse2;
+                            break;
+                        case UIMouseButton.Special0:
+                            mouseCode = KeyCode.Mouse3;
+                            break;
+                        case UIMouseButton.Special1:
+                            mouseCode = KeyCode.Mouse4;
+                            break;
+                        case UIMouseButton.Special2:
+                            mouseCode = KeyCode.Mouse5;
+                            break;
+                        case UIMouseButton.Special3:
+                            mouseCode = KeyCode.Mouse6;
+                            break;
+                        default:
+                            // No valid button pressed: exit without doing anything.
+                            return;
+                    }
+
+                    // We got a valid mouse button key - apply settings and save.
+                    ApplyKey(SavedInputKey.Encode(mouseCode, IsControlDown(), IsShiftDown(), IsAltDown()));
+                }
             }
             else
             {
                 // We weren't already primed - set our text and focus the button.
-                //(mouseEvent.source as UIButton).buttonsMask = (UIMouseButton.Left | UIMouseButton.Right | UIMouseButton.Middle | UIMouseButton.Special0 | UIMouseButton.Special1 | UIMouseButton.Special2 | UIMouseButton.Special3);
+                button.buttonsMask = (UIMouseButton.Left | UIMouseButton.Right | UIMouseButton.Middle | UIMouseButton.Special0 | UIMouseButton.Special1 | UIMouseButton.Special2 | UIMouseButton.Special3);
                 button.text = Translations.Translate("RPR_OPT_PRS");
                 button.Focus();
 
@@ -130,6 +157,25 @@ namespace RealisticPopulationRevisited
 
 
         /// <summary>
+        /// Applies a valid key to our settings.
+        /// </summary>
+        /// <param name="key">InputKey to apply</param>
+        private void ApplyKey(InputKey key)
+        {
+            // Apply key to current settings and save.
+            CurrentHotkey = key;
+            SettingsUtils.SaveSettings();
+
+            // Set the label for the new hotkey.
+            button.text = SavedInputKey.ToLocalizedString("KEYNAME", key);
+
+            // Remove priming.
+            UIView.PopModal();
+            isPrimed = false;
+        }
+
+
+        /// <summary>
         /// Checks to see if the given keycode is a modifier key.
         /// </summary>
         /// <param name="code">Leycode to check</param>
@@ -137,6 +183,37 @@ namespace RealisticPopulationRevisited
         private bool IsModifierKey(KeyCode keyCode)
         {
             return (keyCode == KeyCode.LeftControl || keyCode == KeyCode.RightControl || keyCode == KeyCode.LeftShift || keyCode == KeyCode.RightShift || keyCode == KeyCode.LeftAlt || keyCode == KeyCode.RightAlt || keyCode == KeyCode.AltGr);
+        }
+
+
+        /// <summary>
+        /// Checks to see if the control key is pressed.
+        /// </summary>
+        /// <returns>True if the control key is down, false otherwise.</returns>
+        private bool IsControlDown()
+        {
+            return Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+        }
+
+
+        /// <summary>
+        /// Checks to see if the shift key is pressed.
+        /// </summary>
+        /// <returns>True if the shift key is down, false otherwise.</returns>
+        private bool IsShiftDown()
+        {
+            return Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        }
+
+
+        /// <summary>
+        /// Checks to see if the alt key is pressed.
+        /// </summary>
+        /// <returns>True if the alt key is down, false otherwise.</returns>
+        private bool IsAltDown()
+        {
+            // Don't worry, Alt.Gr, I still remember you, even if everyone else forgets!
+            return Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt) || Input.GetKey(KeyCode.AltGr);
         }
     }
 }
