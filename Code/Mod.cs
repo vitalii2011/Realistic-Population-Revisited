21f44d2e21d3565ac83f6ea1e32334d16e2559f3
algernon <aeneas@aloku.net>
2020-07-06 11:17:01 +1000

    Prepare for 1.4.3 release.

--- Code/Mod.cs
+++ Code/Mod.cs
@@ -7,7 +7,7 @@ namespace RealisticPopulationRevisited
     public class RealPopMod : IUserMod
     {
         public static string ModName => "Realistic Population Revisited";
-        public static string Version => "1.4.2";
+        public static string Version => "1.4.3";
 
         public string Name => ModName + " " + Version;
 
