bea650b9d6f0ef52722d77e68b00ea93b1970280
algernon-A <aeneas@aloku.net>
2021-02-21 12:42:51 +1100

    Move CalculateHomeCount from legacy to new patch structure.

--- Code/AI_Files/AI_Residential.cs
+++ Code/AI_Files/AI_Residential.cs
@@ -9,55 +9,6 @@ using HarmonyLib;
 
 namespace RealPop2
 {
-    /// <summary>
-    /// Harmony patch to implement population count changes for residential buildings.
-    /// </summary>
-    [HarmonyPatch(typeof(ResidentialBuildingAI))]
-    [HarmonyPatch("CalculateHomeCount")]
-    [HarmonyPatch(new Type[] { typeof(ItemClass.Level), typeof(Randomizer), typeof(int), typeof(int) })]
-    public static class RealisticHomeCount
-    {
-        /// <summary>
-        /// Harmony Prefix patch to ResidentialBuildingAI.CalculateHomeCount to implement mod population calculations.
-        /// </summary>
-        /// <param name="__result">Original method result</param>
-        /// <param name="__instance">Original AI instance reference</param>
-        /// <param name="level">Building level</param>
-        /// <param name="r">Randomizer (unused)</param>
-        /// <param name="width">Building lot width (unused)</param>
-        /// <param name="length">Building lot length (unused)</param>
-        /// <returns>Always false (don't execute base game method after this)</returns>
-        public static bool Prefix(ref int __result, ResidentialBuildingAI __instance, ItemClass.Level level, Randomizer r, int width, int length)
-        {
-            // BuildingInfo prefab for this building.
-            BuildingInfo info = __instance.m_info;
-
-            // Check to see if there's a record for this prefab in the population cache.
-            if (PopData.instance.householdCache.ContainsKey(info))
-            {
-                // Yes - get cached value for this level for this prefab.
-                int returnValue = PopData.instance.householdCache[info][(int)level];
-
-                // Check to see if a valid cached (non-zero) result was returned.
-                if (returnValue != 0)
-                {
-                    // Valid return value - set original method return value to this.
-                    __result = returnValue;
-
-                    // Return, and don't execute base method after this.
-                    return false;
-                }
-            }
-
-            // If we got here, there was no valid record in cache - add new cache record and assign original method return value.
-            __result = PopData.instance.CacheHouseholds(info, (int)level);
-
-            // Don't execute base method after this.
-            return false;
-        }
-    }
-
-
     [HarmonyPatch(typeof(ResidentialBuildingAI))]
     [HarmonyPatch("GetConsumptionRates")]
     [HarmonyPatch(new Type[] { typeof(ItemClass.Level), typeof(Randomizer), typeof(int), typeof(int), typeof(int), typeof(int), typeof(int), typeof(int), typeof(int) },
