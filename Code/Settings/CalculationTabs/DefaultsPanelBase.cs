db3b5a056035e241d9d90001604848d6efbbf916
algernon-A <aeneas@aloku.net>
2021-03-13 20:16:50 +1100

    Ensure consistent inheritence for defaults panel apply methods.

--- Code/Settings/CalculationTabs/DefaultsPanelBase.cs
+++ Code/Settings/CalculationTabs/DefaultsPanelBase.cs
@@ -284,6 +284,44 @@ namespace RealPop2
             }
         }
 
+
+        /// <summary>
+        /// 'Save and apply' button event handler.
+        /// </summary>
+        /// <param name="control">Calling component (unused)</param>
+        /// <param name="mouseEvent">Mouse event (unused)</param>
+        protected virtual void Apply(UIComponent control, UIMouseEventParameter mouseEvent)
+        {
+            // Iterate through each sub-service menu.
+            for (int i = 0; i < SubServiceNames.Length; ++i)
+            {
+                // Get population pack menu selected index.
+                int popIndex = popMenus[i].selectedIndex;
+
+                // Check to see if this is a change from the current default.
+                if (!PopData.instance.CurrentDefaultPack(Services[i], SubServices[i]).name.Equals(availablePopPacks[i][popIndex].name))
+                {
+                    // Default has changed - update default population dictionary for this subservice.
+                    PopData.instance.ChangeDefault(Services[i], SubServices[i], availablePopPacks[i][popIndex]);
+                }
+
+                // Update floor data pack if we're not using legacy calculations.
+                if (availablePopPacks[i][popIndex].version != (int)DataVersion.legacy)
+                {
+                    // Check to see if this is a change from the current default.
+                    if (!FloorData.instance.CurrentDefaultPack(Services[i], SubServices[i]).name.Equals(availableFloorPacks[floorMenus[i].selectedIndex]))
+                    {
+                        // Default has changed - update default floor dictionary for this subservice.
+                        FloorData.instance.ChangeDefault(Services[i], SubServices[i], availableFloorPacks[floorMenus[i].selectedIndex]);
+                    }
+                }
+            }
+
+            // Save settings.
+            ConfigUtils.SaveSettings();
+        }
+
+
         /// <summary>
         /// 'Revert to saved' button event handler.
         /// </summary>
--- Code/Settings/CalculationTabs/EmpDefaultsPanel.cs
+++ Code/Settings/CalculationTabs/EmpDefaultsPanel.cs
@@ -37,32 +37,9 @@ namespace RealPop2
         /// </summary>
         /// <param name="control">Calling component (unused)</param>
         /// <param name="mouseEvent">Mouse event (unused)</param>
-        protected virtual void Apply(UIComponent control, UIMouseEventParameter mouseEvent)
+        protected override void Apply(UIComponent control, UIMouseEventParameter mouseEvent)
         {
-            // Iterate through each sub-service menu.
-            for (int i = 0; i < SubServiceNames.Length; ++i)
-            {
-                // Get population pack menu selected index.
-                int popIndex = popMenus[i].selectedIndex;
-
-                // Check to see if this is a change from the current default.
-                if (!PopData.instance.CurrentDefaultPack(Services[i], SubServices[i]).name.Equals(availablePopPacks[i][popIndex].name))
-                {
-                    // Default has changed - update default population dictionary for this subservice.
-                    PopData.instance.ChangeDefault(Services[i], SubServices[i], availablePopPacks[i][popIndex]);
-                }
-
-                // Update floor data pack if we're not using legacy calculations.
-                if (availablePopPacks[i][popIndex].version != (int)DataVersion.legacy)
-                {
-                    // Check to see if this is a change from the current default.
-                    if (!FloorData.instance.CurrentDefaultPack(Services[i], SubServices[i]).name.Equals(availableFloorPacks[floorMenus[i].selectedIndex]))
-                    {
-                        // Default has changed - update default floor dictionary for this subservice.
-                        FloorData.instance.ChangeDefault(Services[i], SubServices[i], availableFloorPacks[floorMenus[i].selectedIndex]);
-                    }
-                }
-            }
+            base.Apply(control, mouseEvent);
 
             // Clear population caches.
             PopData.instance.workplaceCache.Clear();
@@ -72,9 +49,6 @@ namespace RealPop2
             {
                 ModUtils.ricoClearAllWorkplaces.Invoke(null, null);
             }
-
-            // Save settings.
-            ConfigUtils.SaveSettings();
         }
     }
 }
