ebd7da9d65a5eb375b0798ae20db10e40b9202cc
algernon-A <aeneas@aloku.net>
2021-04-24 14:26:29 +1000

    Adjust commercial visitor display.

--- Code/Patches/ZonedBuildingWorldInfoPanel.cs
+++ Code/Patches/ZonedBuildingWorldInfoPanel.cs
@@ -1,65 +1,96 @@
-﻿using HarmonyLib;
-
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using ColossalFramework;
 using ColossalFramework.UI;
+using HarmonyLib;
 
-using System;
-using System.Linq;
-using System.Runtime.CompilerServices;
 
 namespace RealPop2
 {
     /// <summary>
-    /// Harmony Postfix patch to add vbu.
+    /// Harmony Postfix patch to add visitor count display to commercial info panels.
     /// </summary>
     [HarmonyPatch(typeof(ZonedBuildingWorldInfoPanel), "UpdateBindings")]
     public static class InfoPanelPatch
     {
+        // Visitor label reference.
         public static UILabel visitLabel;
 
+
+        /// <summary>
+        /// Harmony Postfix patch to ZonedBuildingWorldInfoPanel.UpdateBindings to display visitor counts for commercial buildings.
+        /// </summary>
         public static void Postfix()
         {
+            // Currently selected building.
             ushort building = WorldInfoPanel.GetCurrentInstanceID().Building;
 
+            // Create visit label if it isn't already set up.
             if (visitLabel == null)
             {
+                // Get info panel.
                 ZonedBuildingWorldInfoPanel infoPanel = UIView.library.Get<ZonedBuildingWorldInfoPanel>(typeof(ZonedBuildingWorldInfoPanel).Name);
-                visitLabel = UIControls.AddLabel(infoPanel.component, 100f, 280f, "Visitors", textScale: 0.625f);
-                visitLabel.textColor = new UnityEngine.Color32(206, 248, 0, 255);
+
+                // Add current visitor count label.
+                visitLabel = UIControls.AddLabel(infoPanel.component, 65f, 280f, Translations.Translate("RPR_INF_VIS"), textScale: 0.75f);
+                visitLabel.textColor = new Color32(185, 221, 254, 255);
                 visitLabel.font = Resources.FindObjectsOfTypeAll<UIFont>().FirstOrDefault((UIFont f) => f.name == "OpenSans-Regular");
 
+                // Position under existing Highly Educated workers count row in line with total workplace count label.
+                UIComponent situationLabel = infoPanel.Find("WorkSituation");
                 UIComponent workerLabel = infoPanel.Find("HighlyEducatedWorkers");
-                if (workerLabel != null)
+                if (situationLabel != null && workerLabel != null)
                 {
-                    visitLabel.absolutePosition = new Vector2(workerLabel.absolutePosition.x - visitLabel.width, workerLabel.absolutePosition.y + 28f);
+                    visitLabel.absolutePosition = new Vector2(situationLabel.absolutePosition.x, workerLabel.absolutePosition.y + 25f);
                 }
-
+                else
+                {
+                    Logging.Error("couldn't find ZonedBuildingWorldInfoPanel components");
+                }
             }
 
+            // Local references.
             Building[] buildingBuffer = Singleton<BuildingManager>.instance.m_buildings.m_buffer;
             BuildingInfo buildingInfo = buildingBuffer[building].Info;
-            CommercialBuildingAI comAI = buildingInfo.GetAI() as CommercialBuildingAI;
 
-            if (comAI == null)
+            // Is this a commercial building?
+            CommercialBuildingAI commercialAI = buildingInfo.GetAI() as CommercialBuildingAI;
+            if (commercialAI == null)
             {
+                // Not a commercial building - hide the label.
                 visitLabel.Hide();
             }
             else
             {
+                // Commercial building - show the label.
                 visitLabel.Show();
+
+                // Get current visitor count.
                 int aliveCount = 0, totalCount = 0;
-                Citizen.BehaviourData behaviour =new Citizen.BehaviourData();
-                GetVisitBehaviour(comAI, building, ref buildingBuffer[building], ref behaviour, ref aliveCount, ref totalCount);
-                visitLabel.text = "Visitors " + totalCount + " / " + comAI.CalculateVisitplaceCount((ItemClass.Level)buildingBuffer[building].m_level, new ColossalFramework.Math.Randomizer(building), buildingBuffer[building].Width, buildingBuffer[building].Length).ToString();
+                Citizen.BehaviourData behaviour = new Citizen.BehaviourData();
+                GetVisitBehaviour(commercialAI, building, ref buildingBuffer[building], ref behaviour, ref aliveCount, ref totalCount);
+
+                // Display visitor count.
+                visitLabel.text = totalCount.ToString() + " / " + commercialAI.CalculateVisitplaceCount((ItemClass.Level)buildingBuffer[building].m_level, new ColossalFramework.Math.Randomizer(building), buildingBuffer[building].Width, buildingBuffer[building].Length).ToString() +  " " + Translations.Translate("RPR_INF_VIS");
             }
         }
 
 
+        /// <summary>
+        /// Reverse patch for CommonBuildingAI.GetVisitBehaviour to access private method of original instance.
+        /// </summary>
+        /// <param name="instance">Object instance</param>
+        /// <param name="citizenID">ID of this building (for game method)</param>
+        /// <param name="buildingData">Building data reference (for game method)</param>
+        /// <param name="behaviour">Citizen behaviour reference (for game method)</param>
+        /// <param name="aliveCount">Alive citizen count</param>
+        /// <param name="totalCount">Total citizen count</param>
         [HarmonyReversePatch]
         [HarmonyPatch((typeof(CommonBuildingAI)), "GetVisitBehaviour")]
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public static void GetVisitBehaviour(CommonBuildingAI instance, ushort buildingID, ref Building buildingData, ref Citizen.BehaviourData behaviour, ref int aliveCount, ref int totalCount)
+        public static void GetVisitBehaviour(object instance, ushort buildingID, ref Building buildingData, ref Citizen.BehaviourData behaviour, ref int aliveCount, ref int totalCount)
         {
             string message = "GetVisitBehaviour reverse Harmony patch wasn't applied";
             Logging.Error(message, instance.ToString(), buildingID.ToString(), buildingData.ToString(), behaviour.ToString(), aliveCount.ToString(), totalCount.ToString());
