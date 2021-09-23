48a55f3192fcc97f00c9a3527470d08352bfce0c
algernon-A <aeneas@aloku.net>
2021-09-23 17:44:21 +1000

    Prepare for 2.0rc2.

--- Code/Mod.cs
+++ Code/Mod.cs
@@ -14,7 +14,7 @@ namespace RealPop2
         // Internal and private name and version components.
         internal static string ModName => "Realistic Population 2";
         internal static string Version => BaseVersion + " " + Beta;
-        internal static string Beta => "rc1";
+        internal static string Beta => "rc2";
         private static string BaseVersion => "2.0";
 
 
