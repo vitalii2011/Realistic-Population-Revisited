baa7135e622af48f72b43ad595f878fe28e7a4ca
algernon-A <aeneas@aloku.net>
2021-10-14 18:51:36 +1100

    Convert legacy options panel generation to on-demand.

--- Code/Settings/CalculationTabs/LegacyPanel.cs
+++ Code/Settings/CalculationTabs/LegacyPanel.cs
@@ -9,6 +9,13 @@ namespace RealPop2
     /// </summary>
     internal class LegacyPanel
     {
+        // Status flag.
+        private bool isSetup = false;
+
+        // Panel reference.
+        protected UIPanel panel;
+
+
         /// <summary>
         /// Adds education options tab to tabstrip.
         /// </summary>
@@ -17,37 +24,56 @@ namespace RealPop2
         internal LegacyPanel(UITabstrip parentTabStrip, int tabIndex)
         {
             // Add tab and helper.
-            UIPanel panel = PanelUtils.AddTextTab(parentTabStrip, Translations.Translate("RPR_OPT_LEG"), tabIndex, out UIButton tabButton, 100f);
+            panel = PanelUtils.AddTextTab(parentTabStrip, Translations.Translate("RPR_OPT_LEG"), tabIndex, out UIButton tabButton, 100f);
 
             // Button size and text scale.
             tabButton.textScale = 0.7f;
 
-            // Add tabstrip.
-            UITabstrip childTabStrip = panel.AddUIComponent<UITabstrip>();
-            childTabStrip.relativePosition = new Vector3(0, 0);
-            childTabStrip.size = new Vector2(744f, 725f);
+            // Event handler to set up child tabs when tab is first clicked.
+            tabButton.eventClicked += (control, clickEvent) => Setup();
+        }
 
-            // Tab container (the panels underneath each tab).
-            UITabContainer tabContainer = panel.AddUIComponent<UITabContainer>();
-            tabContainer.relativePosition = new Vector3(0, 30f);
-            tabContainer.size = new Vector3(744f, 720);
-            childTabStrip.tabPages = tabContainer;
 
-            // Add child tabs.
-            LegacyResidentialPanel resPanel = new LegacyResidentialPanel(childTabStrip, 0);
-            new LegacyIndustrialPanel(childTabStrip, 1);
-            new LegacyCommercialPanel(childTabStrip, 2);
-            new LegacyOfficePanel(childTabStrip, 3);
-
-            // Change tab size and text scale (to differentiate from 'main' tabstrip).
-            foreach (UIButton button in childTabStrip.components)
+        /// <summary>
+        /// Performs initial setup; called when panel first becomes visible.
+        /// </summary>
+        private void Setup()
+        {
+            // Don't do anything if already set up.
+            if (!isSetup)
             {
-                button.textScale = 0.8f;
-                button.width = 100f;
-            }
+                // Perform initial setup.
+                isSetup = true;
+                Logging.Message("setting up ", this.GetType().ToString());
 
-            // Setup residental panel (initial selection).
-            resPanel.Setup();
+                // Add tabstrip.
+                UITabstrip childTabStrip = panel.AddUIComponent<UITabstrip>();
+                childTabStrip.relativePosition = new Vector3(0, 0);
+                childTabStrip.size = new Vector2(744f, 725f);
+
+                // Tab container (the panels underneath each tab).
+                UITabContainer tabContainer = panel.AddUIComponent<UITabContainer>();
+                tabContainer.relativePosition = new Vector3(0, 30f);
+                tabContainer.size = new Vector3(744f, 720);
+                childTabStrip.tabPages = tabContainer;
+
+                // Add child tabs.
+                LegacyResidentialPanel resPanel = new LegacyResidentialPanel(childTabStrip, 0);
+                new LegacyIndustrialPanel(childTabStrip, 1);
+                new LegacyCommercialPanel(childTabStrip, 2);
+                new LegacyOfficePanel(childTabStrip, 3);
+
+                // Change tab size and text scale (to differentiate from 'main' tabstrip).
+                foreach (UIButton button in childTabStrip.components)
+                {
+                    button.textScale = 0.8f;
+                    button.width = 100f;
+                }
+
+                // Perform setup of residential tab (default selection).
+                resPanel.Setup();
+                childTabStrip.selectedIndex = 0;
+            }
         }
     }
 }
